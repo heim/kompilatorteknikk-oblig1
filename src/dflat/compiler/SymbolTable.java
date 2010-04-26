7f89b8cfa0247afc6c4518b8e6299425c009e0c3
Andreas Heim <andreas@heim.no>
2010-04-26 21:36:00 +0200

    added symbolalreadydeclaredexception

--- src/dflat/compiler/SymbolTable.java
+++ src/dflat/compiler/SymbolTable.java
@@ -2,18 +2,47 @@ package dflat.compiler;
 
 import dflat.syntaxtree.type.Name;
 import dflat.syntaxtree.type.Type;
+import dflat.syntaxtree.compiler.exceptions.SymbolAlreadyDeclaredException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 
 
 public class SymbolTable {
+    Stack<Map<Name, Type>> scopeStack;
+    Map<Name, Type> currentScope;
+
+    public SymbolTable() {
+        scopeStack = new Stack<Map<Name, Type>>();
+        currentScope = new HashMap<Name, Type>();
+        scopeStack.push(currentScope);
+    }
+
+
+    public Type lookup(Name name) {
+
+        for(Map<Name, Type> scope : scopeStack) {
+            Type type = scope.get(name);
+            if(type != null) return type;
+        }
+        return null;
+    }
+
     public void insert(Name name, Type type) {
-        //TODO: implement
+        if(currentScope.get(name) != null) {
+            throw new SymbolAlreadyDeclaredException(name);
+        }
+
+        currentScope.put(name, type);
     }
 
     public void enter_scope() {
+        currentScope = new HashMap<Name, Type>();
+        scopeStack.push(currentScope);
     }
 
     public void exit_scope() {
-
-        
+        currentScope = scopeStack.pop();
     }
 }
--- src/dflat/syntaxtree/type/Name.java
+++ src/dflat/syntaxtree/type/Name.java
@@ -2,7 +2,7 @@ package dflat.syntaxtree.type;
 
 import dflat.syntaxtree.Node;
 
-public class Name extends Node {
+public class Name extends Node implements Comparable<Name> {
 
 	private String name;
 
@@ -14,5 +14,9 @@ public class Name extends Node {
 	public String printAst(int indent) {
 		return indentTabs(indent) + "(NAME " + name + ")";
 	}
+
+    public int compareTo(Name o) {
+        return this.name.compareTo(o.name);
+    }
 
 }
