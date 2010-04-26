c3eba704dc903e73c402a1cbb37786f5c92b394f
Andreas Heim <andreas@heim.no>
2010-04-26 22:46:46 +0200

    lagt til bedre print-ast

--- src/dflat/syntaxtree/type/VoidType.java
+++ src/dflat/syntaxtree/type/VoidType.java
@@ -5,13 +5,12 @@ package dflat.syntaxtree.type;
  * User: andreas
  * Date: Apr 25, 2010
  * Time: 4:36:08 PM
- * To change this template use File | Settings | File Templates.
  */
 public class VoidType extends Type {
     
     @Override
     public String printAst(int indent) {
-        return indentTabs(indent) + "void";  //To change body of implemented methods use File | Settings | File Templates.
+        return indentTabs(indent) + "(TYPE void)";  
     }
 
 
